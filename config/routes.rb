82cb2171fdd73de9a71ddd4b5e3cd0f32a9a3fff
shawncodes <shawn@huthunt.com>
2018-08-03 13:05:52 -0400

    controller setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get 'users/index'
+      get 'users/show'
+      get 'users/create'
+      get 'users/update'
+      get 'users/delete'
+    end
+  end
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :users
+  #   end
+  # end
 end

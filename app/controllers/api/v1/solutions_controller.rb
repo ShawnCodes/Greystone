8c9042c20fd39dad9e19945fda288ed16f784df9
shawncodes <shawn@huthunt.com>
2018-08-03 16:00:29 -0400

    completing output calculations

--- app/controllers/api/v1/solutions_controller.rb
+++ app/controllers/api/v1/solutions_controller.rb
@@ -1,7 +1,23 @@
 class Api::V1::SolutionsController < ApplicationController
+  before_action :find_user, only: [:show]
+
   def index
   end
 
   def show
+    total_annual_rent = @user.roll.rent * 12
+    total_expenses = @user.expense.values.reduce(:+) # add all values in a particular user's expense table
+    net_operating_income = total_annual_rent - total_expenses
+
+    #calculations....
+    
+
+    output = {loan_amount: 123, debt_rate: 123}
+    render json: output
+  end
+
+  private
+  def find_user
+    @user = User.find(params: [:id])
   end
 end

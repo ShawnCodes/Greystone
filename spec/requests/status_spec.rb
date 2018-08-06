6448ecdf7aa44ebc937865ee006858edaeaae163
shawncodes <shawn@huthunt.com>
2018-08-06 08:08:26 -0400

    adding final api endpoint tests

--- spec/requests/status_spec.rb
+++ spec/requests/status_spec.rb
@@ -40,3 +40,53 @@ RSpec.describe 'Status requests' do
             }      end
           end
         end
+
+        RSpec.describe 'Status requests' do
+          describe 'GET /capitalizations' do
+            it 'returns a status message' do
+              RestClient.get('http://localhost:3000/api/v1/capitalizations') { |response, request, result, &block|
+                expect(response.code).to eql(200)
+
+              }      end
+            end
+          end
+
+          RSpec.describe 'Status requests' do
+            describe 'POST /capitalizations' do
+              it 'returns a status message' do
+                RestClient.get('http://localhost:3000/api/v1/capitalizations') { |response, request, result, &block|
+                  expect(response.code).to eql(200)
+
+                }      end
+              end
+            end
+
+            RSpec.describe 'Status requests' do
+              describe 'GET /users' do
+                it 'returns a status message' do
+                  RestClient.get('http://localhost:3000/api/v1/users') { |response, request, result, &block|
+                    expect(response.code).to eql(200)
+
+                  }      end
+                end
+              end
+
+              RSpec.describe 'Status requests' do
+                describe 'POST /users' do
+                  it 'returns a status message' do
+                    RestClient.get('http://localhost:3000/api/v1/users') { |response, request, result, &block|
+                      expect(response.code).to eql(200)
+
+                    }      end
+                  end
+                end
+
+                RSpec.describe 'Status requests' do
+                  describe 'GET /solutions' do
+                    it 'returns a status message' do
+                      RestClient.get('http://localhost:3000/api/v1/solutions') { |response, request, result, &block|
+                        expect(response.code).to eql(204)
+
+                      }      end
+                    end
+                  end

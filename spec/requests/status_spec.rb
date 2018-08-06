d1a19687ed9f0a0740783a76155fae8bd591ea4b
shawncodes <shawn@huthunt.com>
2018-08-06 07:52:56 -0400

    adding additional endpoint tests

--- spec/requests/status_spec.rb
+++ spec/requests/status_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'rest-client'
 
 RSpec.describe 'Status requests' do
-  describe 'GET /inputs' do
+  describe 'GET /rolls' do
     it 'returns a status message' do
       RestClient.get('http://localhost:3000/api/v1/rolls') { |response, request, result, &block|
         expect(response.code).to eql(200)
@@ -12,7 +12,7 @@ RSpec.describe 'Status requests' do
   end
 
   RSpec.describe 'Status requests' do
-    describe 'POST /inputs' do
+    describe 'POST /rolls' do
       it 'returns a status message' do
         RestClient.get('http://localhost:3000/api/v1/rolls') { |response, request, result, &block|
           expect(response.code).to eql(200)

ed7813016895a8d758c8f249faea396d8eadbda6
shawncodes <shawn@huthunt.com>
2018-08-12 18:00:16 -0400

    additional refactoring

--- app/controllers/api/v1/rolls_controller.rb
+++ app/controllers/api/v1/rolls_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::RollsController < ApplicationController
+  before_action :find_user, only: [:create, :update]
   # Since in this case there's only one user, we make all the post fetch to `/api/v1/rolls/1`
   # Assuming from a post fetch, we receive the following params: {id: 1, roll: {address: '', unit: '', vacancy: '', monthly: 123, bedrooms: 123, bathrooms: 123, annual: 123}}
 
@@ -12,13 +13,13 @@ class Api::V1::RollsController < ApplicationController
   end
 
   def create
-    @user = User.find(params[:id]) # In our case id = 1
+    # @user = User.find(params[:id]) # In our case id = 1
     @roll = @user.roll.create(address: params[:roll][:address], unit: params[:roll][:unit], vacancy: params[:roll][:vacancy], monthly: params[:roll][:monthly], bedrooms: params[:roll][:bedrooms], bathrooms: params[:roll][:bathrooms], annual: params[:roll][:annual])
     # render json: @roll
   end
 
   def update
-    @user = User.find(params[:id]) # In our case id = 1
+    # @user = User.find(params[:id]) # In our case id = 1
     @roll = @user.roll
     @roll.update(address: params[:roll][:address], unit: params[:roll][:unit], vacancy: params[:roll][:vacancy], monthly: params[:roll][:monthly], bedrooms: params[:roll][:bedrooms], bathrooms: params[:roll][:bathrooms], annual: params[:roll][:annual])
     @roll.save
@@ -28,5 +29,10 @@ class Api::V1::RollsController < ApplicationController
   def destroy
     @roll = User.find(params[:id]).roll
     @roll.destroy
+  end
+
+  private
+  def find_user
+    @user = User.find(params[:id])
   end
 end
